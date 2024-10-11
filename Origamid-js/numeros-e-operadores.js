9efa69846dbee8b9a29e673eecae00f1f8fab45e
jefersonbeckmann <63985906+jefersonbeckmann@users.noreply.github.com>
2024-10-11 18:54:03 -0300

    Update_11.10.24

--- Origamid-js/numeros-e-operadores.js
+++ Origamid-js/numeros-e-operadores.js
@@ -7,7 +7,29 @@ console.log(modulo);
 var testeNaN = "100";
 console.log(isNaN(testeNaN));
 
-var numero = 80;
+var numero = +"80";
 var unidade = "kg";
 var peso = numero + unidade; // '80kg'
 var pesoPorDois = peso / 2; // NaN(Not a Number)
+console.log(+numero / 2);
+var incremento = 5;
+console.log(incremento++);
+console.log(incremento);
+
+var incremento2 = 5;
+console.log(++incremento);
+console.log(incremento);
+
+var strPnumber = +"5";
+
+// Qual o resultado da seguinte operação?
+var total2 = 10 + (5 * 2) / 2 + 20;
+console.log(total2);
+var NaN = "fui100";
+var NaN2 = "era200";
+var str = "200";
+var num = 50;
+console.log(+str + num);
+var i = 5;
+console.log(i++);
+console.log(i);
