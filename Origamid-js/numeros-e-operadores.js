0cb09014e4c1a2314eaf1f5124666124df260145
jefersonbeckmann <63985906+jefersonbeckmann@users.noreply.github.com>
2024-10-11 17:15:34 -0300

    Numeros e Operadores
    Aula 3 do Curso

---
+++ Origamid-js/numeros-e-operadores.js
@@ -0,0 +1,13 @@
+var total1 = 10 + 5 + 10,
+  divisão = 100 / 5,
+  modulo = 34567890 % 2;
+
+console.log(modulo);
+
+var testeNaN = "100";
+console.log(isNaN(testeNaN));
+
+var numero = 80;
+var unidade = "kg";
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2; // NaN(Not a Number)
